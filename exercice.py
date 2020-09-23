96d855f50aac77a3cc7de7e64e74ea3e59b69c27
heimanu tepu <42362243+manuts987@users.noreply.github.com>
2020-09-23 13:49:35 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -6,25 +6,47 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    nombre = input('donner un nombre ')
+    nombre_entier = float(nombre)
+    valeur_absolue = abs(nombre_entier)
+    print(valeur_absolue)
+    return valeur_absolue
 
 
 def use_prefixes() -> List[str]:
-    prefixes, suffixes = 'JKLMNOP', 'ack'
+    prefixes, suffixes = 'JKLMNOPQ', 'ack'
+    Les_noms = 0
+    for i in (prefixes):
+        print(i + suffixes)
 
-    return [""]
+    return [letter + suffixes for letter in prefixes]
 
 
 def prime_integer_summation() -> int:
-    return 0
+    somme = 0
+    for i in range(1,100,1):
+        if i%2!=0:
+            somme += i
+        else:
+            continue
+
+    return somme
 
 
 def factorial(number: int) -> int:
-    return 0
+    for i in range(number,1, -1):
+        factorial = i*(i-1)
+    return factorial
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1,10,1):
+        if i == 5:
+            continue
+        else:
+            print(i)
+
+
 
 
 def main() -> None:
